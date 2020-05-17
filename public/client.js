a2679453a6b6a33898eb7d36dae79a9770088481
Terry Chen <tyxchen@outlook.com>
2020-05-17 17:18:14 -0400

    web app?

--- public/client.js
+++ public/client.js
@@ -7,21 +7,15 @@
 $(function() {
   console.log('hello world :o');
   
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
+  function getExcuses() {
+    $.get('/excuses', function(excuse) {
+      $('#excuse').text(excuse);
     });
-  });
+  };
 
-  $('form').submit(function(event) {
-    event.preventDefault();
-    dream = $('input').val();
-    $.post('/dreams?' + $.param({'dream': dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
+  $('#new-excuse').click(getExcuses);
+
+  getExcuses();
 
 });
 
