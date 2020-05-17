dee56182ba60a0f7f0ab61de092400458219d765
Terry Chen <tyxchen@outlook.com>
2020-05-17 15:25:43 -0400

    Use ML to conjugate verbs
    this is great
    
    'My aboleth destroyed a zoo.'
    'My death dog drank to its death.'
    'if I open my camera umber hulks from the nether will come and bang me.'
    'Internet Explorer doesn't support marrying right now.'

---
+++ excuses.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import inflect
+import nltk
+import json
+import random
+import re
+import mlconjug
+
+SCHEMA = [
+        "My [ORGANISM] [ACTION:ps] [COMMONOBJECT:a].",
+    "My [ORGANISM] [ACTION_OBJECT:ps] to its death.",
+    "if I open my camera [ORGANISM:p] from [PLACE] will come and [ACTION] me.",
+    "Internet Explorer doesn't support [ACTION:pc] right now.",
+]
+
+p = inflect.engine()
+p.classical(zero=False,herd=True,persons=False,ancient=False)
+conjug = mlconjug.Conjugator(language='en')
+
+def pluralize(word):
+    noun = word
+    # do some nlp to determine what to pluralize
+    if ' ' in word:
+        noun = [word for (word, pos) in nltk.pos_tag(nltk.word_tokenize(word)) if pos[:2] == 'NN'][-1]
+    return re.sub(noun, p.plural(noun), word)
+        
+
+def excuse_from_scheme(scheme, d):
+    def replace(match):
+        flags = set(match[2].split(':')) if match[2] is not None else []
+        sub = random.choice(tuple(d[match[1]])).lower()
+
+        if 'a' in flags: # a/an
+            sub = p.a(sub)
+        if 'p' in flags: # pluralize
+            sub = pluralize(sub)
+        if 'ps' in flags: # past simple
+            sub = conjug.conjugate(sub).conjug_info["indicative"]["indicative past tense"]["1p"]
+        if 'pc' in flags: # present continuous
+            sub = conjug.conjugate(sub).conjug_info["indicative"]["indicative present continuous"]["1p 1p"]
+
+        return sub
+
+    return re.sub(r"\[([\w_]+)((:\w+)*)\]", replace, scheme)
+
+if __name__ == "__main__":
+    with open("words.json", "r") as f:
+        d = json.load(f)
+
+        for scheme in SCHEMA:
+            print(excuse_from_scheme(scheme, d))
